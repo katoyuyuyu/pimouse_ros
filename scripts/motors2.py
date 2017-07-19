39dba5df1b3200485b37b6107c43b37159512c71
e1211205 <ihensyu@gmail.com>
2017-07-19 04:48:13 +0000

    Add_node

--- scripts/motors2.py
+++ scripts/motors2.py
@@ -14,6 +14,7 @@ class Motor():
         self.sub_cmd_vel = rospy.Subscriber('cmd_vel', Twist, self.callback_cmd_vel)
         self.srv_on = rospy.Service('motor_on', Trigger, self.callback_on)
         self.srv_off = rospy.Service('motor_off', Trigger, self.callback_off)
+        self.srv_tm = rospy.Service('timed_motion', TimedMotion, self.callback_tm)
         self.last_time = rospy.Time.now()
         self.using_cmd_vel = False
 
@@ -60,6 +61,22 @@ class Motor():
 
     def callback_on(self,message): return self.onoff_response(True)
     def callback_off(self,message): return self.onoff_response(False)
+
+    def callback_tm(self,message):
+        if not self.is_on:
+            rospy.logger("not enpowered")
+            return False
+
+        dev = "/dev/rtmotor0"
+        try:
+            with open(dev,'w') as f:
+                f.write("%d %d %d \n" %
+                    (message.left_hz,message.right_hz,message.duration_ms))
+        except:
+            rospy.logerr("cannot write to " + dev)
+            return False
+
+        return True
 
 if __name__ == '__main__':
     rospy.init_node('motors')
